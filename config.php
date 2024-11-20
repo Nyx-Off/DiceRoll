95b7f158c410717ad5fb0fa9319e905f2b4d88cb
Samy - Nyx <119658450+Nyx-Off@users.noreply.github.com>
2024-11-20 14:35:51 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php - Configuration de la base de données
-define('DB_HOST', 'zy16r.myd.infomaniak.com');
-define('DB_NAME', 'zy16r_dice_roller');
-define('DB_USER', 'zy16r_system');
-define('DB_PASS', 'SamyBensalem@2024');
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
